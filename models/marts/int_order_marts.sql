7d9203fbfb06b3d43dcd19501f044aa113f3de2e
arkotha9 <akotha@usc.edu>
2024-12-27 20:00:52 -0800

    Created fact tables in models/marts

---
+++ models/marts/int_order_marts.sql
@@ -0,0 +1,16 @@
+SELECT
+    litems.order_item_key,
+    litems.part_key,
+    litems.line_number,
+    orders.order_key,
+    orders.customer_key,
+    orders.order_date,
+    {{ discounted_amt('litems.extended_price', 'litems.discount_percentage')}} as item_discount_amount
+FROM
+    {{ ref('stg_tpch_orders') }} as orders
+JOIN
+    {{ ref('stg_tpch_line_items') }} as litems
+ON
+    orders.order_key = litems.order_key
+ORDER BY
+    orders.order_date
